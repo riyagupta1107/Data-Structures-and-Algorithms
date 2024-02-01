f17a3f60390b5754e9ed85d7f08e10538923490a
riyagupta1107 <riya.gupta070705@gmail.com>
2024-02-01 13:37:41 +0530

    Create insertion-sort.py

---
+++ Sorting-Algorithms/insertion-sort.py
@@ -0,0 +1,13 @@
+#Program to implement insertion sort
+
+def insertionSort(arr):
+    i=1
+    while i<len(arr):
+        j=i-1
+        small=arr[i]
+        while j>=0 and small<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=small
+        i+=1
+    return arr

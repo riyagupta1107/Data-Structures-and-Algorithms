a42bddc3f631bc6a44f0e2c25de3c95ae180bc28
riyagupta1107 <riya.gupta070705@gmail.com>
2024-02-14 22:24:30 +0530

    linked-list-traversal
    recursive and iterative approach to traversal of a linked list

---
+++ Linked Lists/singly-linked-traversal.py
@@ -0,0 +1,38 @@
+class Node:
+
+    # Function to intialize the node object.
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # intialize nect as null
+
+# Linked list class 
+class LinkedList:
+
+    # Function to initialize linked list object
+    def __init__(self):
+        self.head = None 
+
+a = Node('A')
+b = Node('B')
+c = Node('C')
+d = Node('D')
+
+a.next = b
+b.next = c
+c.next = d
+
+# A -> B -> C-> D -> NULL
+
+def printLinkedList(head):
+    current=head
+    while current != None:
+        print(current.data)
+        current=current.next
+
+def recursiveLinkedList(head):
+    if head == None:
+        return 
+    print(head.data)
+    recursiveLinkedList(head.next)
+
+recursiveLinkedList(a)
